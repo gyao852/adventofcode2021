031a59d7742e034db65d02e2915a3db97b561a04
George Yao <gyao@andrew.cmu.edu>
2022-01-02 16:19:24 +0800

    Finished part1, wraping up part2

--- day12/12.go
+++ day12/12.go
@@ -7,6 +7,8 @@ import (
 
 var basePath = "/Users/gyao/Documents/Personal Projects/go_projects/src/2021advent_of_code/day12/"
 
+var COUNT = 0
+
 // PROBLEM-SPECIFIC UTIL FUNCTIONS
 type cave struct {
 	name          string
@@ -26,33 +28,80 @@ func Contains(s []cave, e cave) bool {
 
 func PrettyPrintCaveMap(s map[string]cave) {
 	for k, v := range s {
-		aoc.Log(k)
-		for i, j := range v.adjacentCaves {
-			aoc.Log(i+1, ":", j.name)
+		allPaths := ""
+		for _, j := range v.adjacentCaves {
+			allPaths += j.name + ","
 		}
+		aoc.Log(k, "->", allPaths)
 	}
+	aoc.Log(s)
 }
 
 func IsBigCave(caveName string) bool {
 	return caveName == strings.ToUpper(string(caveName))
 }
 
-func DFS(currPos string, currPath string, adjMap map[string]cave, smallCavesVisited []cave, numOfPaths int) int {
+func CanExploreSmallCave(part1 bool, smallCavesVisited []cave, currCave cave) bool {
+	if part1 {
+		return !Contains(smallCavesVisited, currCave)
+	}
+	return false
+}
+func DFS(currPos string, currPath string, adjMap map[string]cave, smallCavesVisited []cave, part1 bool) {
 	if currPos == "end" {
 		aoc.Log(currPath)
-		return 1
+		COUNT++
 	}
 
 	// For each adjacent cave, we try to explore each of them
 	for _, adjCave := range adjMap[currPos].adjacentCaves {
 		// if we haven't explored an adjacent cave yet, try exploring it
 		if IsBigCave(adjCave.name) {
-			numOfPaths += DFS(adjCave.name, currPath+" -> "+adjCave.name, adjMap, smallCavesVisited, numOfPaths)
-		} else if !Contains(smallCavesVisited, adjCave) {
-			numOfPaths += DFS(adjCave.name, currPath+" -> "+adjCave.name, adjMap, append(smallCavesVisited, adjCave), numOfPaths)
+			DFS(adjCave.name, currPath+" -> "+adjCave.name, adjMap, smallCavesVisited, part1)
+		} else if CanExploreSmallCave(part1, smallCavesVisited, adjCave) {
+			DFS(adjCave.name, currPath+" -> "+adjCave.name, adjMap, append(smallCavesVisited, adjCave), part1)
 		}
 	}
-	return numOfPaths
+}
+
+func PopulateAdjMap(input []string) map[string]cave {
+	numOfCaves := len(input)
+	adjMap := make(map[string]cave, numOfCaves)
+	adjMap["start"] = cave{"start", false, make([]cave, 0)}
+
+	for i := 0; i < numOfCaves; i++ {
+		line := strings.Split(input[i], "-")
+		head := cave{line[0], IsBigCave(line[0]), make([]cave, 0)}
+		tail := cave{line[1], IsBigCave(line[1]), make([]cave, 0)}
+
+		// Add {a : b} mapping
+		if tail.name != "start" && head.name != "end" {
+			if val, ok := adjMap[head.name]; ok {
+				if !Contains(val.adjacentCaves, tail) {
+					val.adjacentCaves = append(val.adjacentCaves, tail)
+					adjMap[head.name] = val
+				}
+			} else {
+				head.adjacentCaves = append(head.adjacentCaves, tail)
+				adjMap[head.name] = head
+			}
+		}
+
+		// Add {b : a} mapping
+		if head.name != "start" && tail.name != "end" {
+			if val, ok := adjMap[tail.name]; ok {
+				if !Contains(val.adjacentCaves, head) {
+					val.adjacentCaves = append(val.adjacentCaves, head)
+					adjMap[tail.name] = val
+				}
+			} else {
+				tail.adjacentCaves = append(tail.adjacentCaves, head)
+				adjMap[tail.name] = tail
+			}
+		}
+
+	}
+	return adjMap
 }
 
 // SOLVER
@@ -61,52 +110,18 @@ func Solve(inputFile string) {
 	// Read input and initialize values
 	result := 0
 	input := aoc.ReadInput(inputFile, "\n")
-	numOfCaves := len(input)
-	adjMap := make(map[string]cave, numOfCaves)
-	// part1 := false
+	adjMap := PopulateAdjMap(input)
+	part1 := true
 
-	for i := 0; i < numOfCaves; i++ {
-		line := strings.Split(input[i], "-")
-		head := cave{line[0], IsBigCave(line[0]), make([]cave, 0)}
-		tail := cave{line[1], IsBigCave(line[1]), make([]cave, 0)}
-
-		// Populate head or tail if not already
-		if val, ok := adjMap[head.name]; ok {
-			if !Contains(val.adjacentCaves, tail) {
-				val.adjacentCaves = append(val.adjacentCaves, tail)
-				adjMap[head.name] = val
-			}
-		} else {
-			head.adjacentCaves = append(head.adjacentCaves, tail)
-			adjMap[head.name] = head
-		}
-
-		if val, ok := adjMap[tail.name]; ok {
-			if head.name == "start" || tail.name == "end" {
-				continue
-			}
-			if !Contains(val.adjacentCaves, head) {
-				val.adjacentCaves = append(val.adjacentCaves, head)
-				adjMap[tail.name] = val
-			}
-		} else {
-			if head.name == "start" || tail.name == "end" {
-				continue
-			}
-			tail.adjacentCaves = append(tail.adjacentCaves, head)
-			adjMap[tail.name] = tail
-		}
-	}
 	PrettyPrintCaveMap(adjMap)
-	result = DFS("start", "start", adjMap, make([]cave, 0), 0)
-	// PrettyPrintCaves(adjMap)
+	DFS("start", "start", adjMap, make([]cave, 0), part1)
+	result = COUNT
 	aoc.Log("result", result)
 }
 
 func main() {
-	Solve(basePath + "input/t.txt")
+	// Solve(basePath + "input/t.txt")
 	// Solve(basePath + "input/t2.txt")
 	// Solve(basePath + "input/t3.txt")
-	// Solve(basePath + "input/test12.txt")
-	// Solve(basePath + "input/12.txt")
+	Solve(basePath + "input/12.txt")
 }
